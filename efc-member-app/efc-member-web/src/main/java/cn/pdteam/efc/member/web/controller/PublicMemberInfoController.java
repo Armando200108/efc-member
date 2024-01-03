7965eea2ec8a400fab3f04ef29036752a23e885a
李东璋 (Armando Lee) <armando20010811@gmail.com>
2024-01-03 19:34:09 +0800

    feat(个人信息): 完善查询个人信息逻辑

--- efc-member-app/efc-member-web/src/main/java/cn/pdteam/efc/member/web/controller/PublicMemberInfoController.java
+++ efc-member-app/efc-member-web/src/main/java/cn/pdteam/efc/member/web/controller/PublicMemberInfoController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.pdteam.efc.component.ResultBase;
+import cn.pdteam.efc.enums.member.IdentityEnum;
+import cn.pdteam.efc.enums.member.QueryInfoPrimaryTypeEnum;
 import cn.pdteam.efc.member.api.PublicMemberInfoApi;
 import cn.pdteam.efc.member.api.request.RegisterMemberRequest;
+import cn.pdteam.efc.member.api.response.QueryMemberInfoResponse;
 import cn.pdteam.efc.member.api.response.RegisterMemberResponse;
 import cn.pdteam.efc.member.business.support.service.StudentInfoService;
 import cn.pdteam.efc.member.business.support.service.TeacherInfoService;
@@ -35,6 +38,36 @@ public class PublicMemberInfoController implements PublicMemberInfoApi {
                     case TEACHER -> teacherInfoService.registerTeacher(request.getMemberId());
                 };
         return ResultBase.success(response, "注册成功");
+    }
+
+    /**
+     * 查询member info
+     *
+     * @param type 类型 01：主键 02：学号/工号
+     * @param id   主键/学号/工号
+     * @return member info
+     */
+    @Override
+    public ResultBase<QueryMemberInfoResponse> query(QueryInfoPrimaryTypeEnum type, String id) {
+        QueryMemberInfoResponse response =
+                switch (type) {
+                    case PRIMARY_ID -> {
+                        long primaryId = Long.parseLong(id);
+                        QueryMemberInfoResponse queryMemberInfoResponse = studentInfoService.queryStudent(primaryId);
+                        if (queryMemberInfoResponse == null) {
+                            queryMemberInfoResponse = teacherInfoService.queryTeacher(primaryId);
+                        }
+                        yield queryMemberInfoResponse;
+                    }
+                    case MEMBER_ID -> {
+                        IdentityEnum identity = IdentityEnum.getIdentity(id);
+                        yield switch (identity) {
+                            case STUDENT -> studentInfoService.queryStudent(id);
+                            case TEACHER -> teacherInfoService.queryTeacher(id);
+                        };
+                    }
+                };
+        return ResultBase.success(response, "查询成功");
     }
 
 }
